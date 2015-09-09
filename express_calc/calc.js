9091cf18b5714fdb4a3a4da463794dd8b3a26056
aaron <harms280@gmail.com>
2015-09-09 16:25:34 -0700

    adjusted for decimal numbers

--- express_calc/calc.js
+++ express_calc/calc.js
@@ -4,16 +4,16 @@ app.set('view engine', 'ejs');
 
 app.get("/:op/:num1/:num2", function(req,res) {
   if(req.params.op === "add") {
-    var sum = parseInt(req.params.num1) + parseInt(req.params.num2);
+    var sum = parseFloat(req.params.num1) + parseFloat(req.params.num2);
     res.render('index',{answer: sum.toString()});
   } else if (req.params.op === "sub") {
-    var sub = parseInt(req.params.num1) - parseInt(req.params.num2);
+    var sub = parseFloat(req.params.num1) - parseFloat(req.params.num2);
     res.render('index',{answer: sub.toString()});
   } else if (req.params.op === "mult") {
-    var mult = parseInt(req.params.num1) * parseInt(req.params.num2);
+    var mult = parseFloat(req.params.num1) * parseFloat(req.params.num2);
     res.render('index',{answer: mult.toString()});
   } else if (req.params.op === "div") {
-    var div = parseInt(req.params.num1) / parseInt(req.params.num2);
+    var div = parseFloat(req.params.num1) / parseFloat(req.params.num2);
     res.render('index',{answer: div.toString()});
   } else {
     res.render('index', {answer: "That ain't right"});
